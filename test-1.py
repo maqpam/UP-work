ef8735f9f07e8e248688f7ae831e8993694c91f1
Paktz <parichkla2487@gmail.com>
2022-03-03 12:01:09 +0700

    added test

---
+++ test-1.py
@@ -0,0 +1,13 @@
+# ข้อสอบปลายภาคข้อที่ 1 
+# ชื่อ ปริชญ์ กองทอง
+# ชั้น ม 6/2
+# เลขที่ 24
+# รหัสประจำตัวนักเรียน 64370
+Num = int(input("number:"))
+Ans = Num%2
+if (Ans == 0):
+    print("EVEN")
+elif (Ans == 1):
+    print("ODD")
+else :
+    print("ERROR")
---
+++ test-2.py
@@ -0,0 +1,13 @@
+# ข้อสอบปลายภาคข้อที่ 2 
+# ชื่อ ปริชญ์ กองทอง
+# ชั้น ม 6/2
+# เลขที่ 24
+# รหัสประจำตัวนักเรียน 64370
+Score1 = int(input("Score_1:"))
+Score2 = int(input("Score_2:"))
+Score3 = int(input("Score_3:"))
+Ans = Score1 + Score2 + Score3
+if (Ans >= 60):
+    print("PASS")
+else :
+    print("FAIL")
